063902b913e5290a72c00ad45c911ec7856381ce
David Van Fleet <dvfleet413@gmail.com>
2019-12-18 21:06:04 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -49,7 +49,7 @@ total = 0
 
 row_index = 0
 while row_index < src.count do
-  if (src[row_index][0] % 2 == 0) && (src[row_index][1] % 2 == 0) do 
+  if (src[row_index][0] % 2 == 0) && (src[row_index][1] % 2 == 0) 
     total = total + src[row_index][0] + src[row_index][1]
   end
 end
